f9d279d98ce655c726eeab120449c04bfa82ee68
albafik jj <esteban.45.bd@gmail.com>
2024-09-07 01:38:18 +0000

    feat: add basic inputs validations

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,17 @@
 #! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=  --tuple-only -c"
+
+NUM_RANDOM=$RANDOM
+
+echo "Enter your username:"
+read USERNAME
+
+GET_GAMERINFO=$()
+
+if [[ -z $GET_GAMERINFO ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  echo "Guess the secret number between 1 adn 1000:"
+  input SECRET_NUMBER
+fi
